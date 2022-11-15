4efe9dcdae6e4b174020095ded389dacd7311ca5
JackJuno <k4cooperation@outlook.com>
2022-11-15 19:55:20 +1000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+# DialogueGetter gets dialogues from russian texts,
+# clean them and write down to a dataset file <.txt>
+# for use in nlp projects.
+import datetime
+import os.path
+from dataclasses import dataclass
+import datetime
+import re
+from tkinter import *
+from tkinter import messagebox
+from tkinter import ttk
+
+# "[\s]*-[\s]*[А-Я]" find -This is a Test sentence.
+
+
+if __name__ == '__main__':
+    # Open main window
+    root = Tk()
+    root.title("Dialogues Getter RU")
+    root.geometry('412x732')
+    root.resizable(0, 0)
+
+
+    # Execute Tkinter
+    root.mainloop()
